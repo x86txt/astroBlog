d9339642d4ff64a6152a85c89b7cd05e5eea70e2
Matt <git@mattevans.cloud>
2026-06-14 17:20:46 -0400

    Changed: migrate markdown remark plugins to unified processor in astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
+import { unified } from "@astrojs/markdown-remark";
 import {
   transformerNotationDiff,
   transformerNotationHighlight,
@@ -28,7 +29,9 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    processor: unified({
+      remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
+    }),
     shikiConfig: {
       // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "github-dark-default" },
